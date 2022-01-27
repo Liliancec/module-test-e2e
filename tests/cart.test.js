8efc2567412d07967c53796fe19041c22f5674d0
lilian <lilian.ceccon@le-campus-numerique.fr>
2022-01-27 09:17:38 +0100

    test

--- tests/cart.test.js
+++ tests/cart.test.js
@@ -11,9 +11,10 @@ describe("Cart features", () => {
         await page.type('#password', process.env.TEST_PASSWORD);
         await page.click('#login-button');
 
-        await page.waitForSelector('#add-to-cart-sauce-labs-backpack');
-        await page.click('#add-to-cart-sauce-labs-backpack')
-        await page.click('.shopping_cart_link')
+        await page.screenshot({path: './tests/img/test_screen.png'})
+        await page.waitFor(1000);
+        await page.click('#add-to-cart-sauce-labs-backpack');
+        await page.click('.shopping_cart_link');
 
         const titre = await page.$eval(".inventory_item_name",(e)=>e.innerText);
         console.log(titre);
--- tests/checkout.test.js
+++ tests/checkout.test.js
@@ -11,7 +11,7 @@ describe("Checkout process", () => {
         await page.type('#password', process.env.TEST_PASSWORD);
         await page.click('#login-button');
 
-        await page.click('#add-to-cart-sauce-labs-backpack')
+        await page.click('#add-to-cart-sauce-labs-bike-light')
         await page.click('.shopping_cart_link')
 
         const titre = await page.$eval(".inventory_item_name",(e)=>e.innerHTML);
